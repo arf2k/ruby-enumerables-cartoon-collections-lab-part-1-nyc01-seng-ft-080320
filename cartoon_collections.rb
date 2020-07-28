10eba56d04876db6bf60b533df93cd6d2ccec84f
Alex Foreman <foreman.alexander@gmail.com>
2020-07-28 18:42:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def greet_characters(array)
 end 
 
 def list_dwarves(array)
-  array.each_with_index do |characters, index|
-    
+  array.each_with_index do |character, index|
+    puts "#{(index +1)}. #{character}"
+  end
+end 
 
 
 
